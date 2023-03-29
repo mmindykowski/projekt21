64650f411ecc564a3f1d58be1d811634a87ffc4c
Mikolaj Mindykowski <mikolaj.mindykowski@gmail.com>
2023-03-29 21:05:44 +0200

    suma zmiennych js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 500;
+
+let secondNumber = 600;
+
+function gross(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+console.log(gross(firstNumber, secondNumber));
